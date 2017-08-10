5724730fc252429bb035cb0a5f9327220d9df0f8
Jared <jpoeppel@microsoft.com>
2017-08-10 18:53:41 -0400

    commit 1 (beta version)

---
+++ test/posh-cef.Tests.ps1
@@ -0,0 +1,10 @@
+$ModuleManifestName = 'posh-cef.psd1'
+$ModuleManifestPath = "$PSScriptRoot\..\$ModuleManifestName"
+
+Describe 'Module Manifest Tests' {
+    It 'Passes Test-ModuleManifest' {
+        Test-ModuleManifest -Path $ModuleManifestPath
+        $? | Should Be $true
+    }
+}
+
