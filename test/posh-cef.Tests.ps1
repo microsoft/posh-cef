961ea9f4e4175c5595a1f44267027bd9cf266a0a
Jared <jpoeppel@microsoft.com>
2017-08-14 16:28:24 -0400

    Restoring test script to normal

--- test/posh-cef.Tests.ps1
+++ test/posh-cef.Tests.ps1
@@ -1,4 +1,4 @@
-<#
+
 $ModuleManifestName = 'posh-cef.psd1'
 $ModuleManifestPath = "$PSScriptRoot\..\$ModuleManifestName"
 
@@ -8,8 +8,9 @@ Describe 'Module Manifest Tests' {
         $? | Should Be $true
     }
 }
-#>
 
+
+<#
 
 $Collection = @()
 
@@ -24,7 +25,7 @@ $Collection | New-CEFMessage -DeviceVendor 'JaredP' -DeviceProduct 'MyScript' -D
 
 
 New-CEFMessage -DeviceVendor 'Contoso' -DeviceProduct 'MyPowershellScript' -DeviceVersion '1.0' -DeviceEventClassId 'Alert' -Name 'Bad Thing Detected' -Severity 10 -externalId 12345 -dmac '01-23-45-67-89-AF' -src 192.168.1.1 -deviceDirection Outbound -spriv Administrator -Type Base -In 6213467 -cfp1 3.141592653589 -CustomExtensionRawString 'key=value' |  Write-Host -ForegroundColor Cyan
-
+#>
 
 
 
